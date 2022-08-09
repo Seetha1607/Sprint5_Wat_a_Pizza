74462144a284db9250809fca30655b05f1e007c7
seethalakshmi1607 <seethalakshmi1607@gmail.com>
2022-08-09 15:14:21 +0000

    Saving changes from StackRoute workbench on Tue Aug 09 2022 20:44:20 GMT+0530 (India Standard Time)

--- PizzaOrder.java
+++ PizzaOrder.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
-class PizzaOrder {
-    void displayCustomerDetails(String name, String email, long phoneNumber, String address) {
-    }
 
+class PizzaOrder {
+    void displayCustomerDetails(String customerName, String customerEmail, long phoneNumber, String customerAddress) {
+        System.out.println("Name of the Customer is : " + customerName);
+        System.out.println("Email of the Customer is : " + customerEmail);
+        System.out.println("Contact no of the Customer is :" + phoneNumber);
+        System.out.println("Address of the Customer is : " + customerAddress);
+    }
     void displayMenu() {
+        System.out.println("Sl no  Category");
+        System.out.println("1)    Pizza");
+        System.out.println("2)    Garlic Bread");
+        System.out.println("3)    Beverage");
     }
     public float getPriceOfPizzaBasedOnSize(int size) {
         float priceOfPizza = 0;
@@ -42,92 +50,79 @@ class PizzaOrder {
 
     public float calculateTotalBill(float totalPizzaPrice, float totalGarlicBreadPrice, float totalBeveragePrice) {
         float totalBillAmount = totalPizzaPrice + totalGarlicBreadPrice + totalBeveragePrice;
-        System.out.println("The Total Bill Amount is : " + "$" + totalBillAmount);
         return totalBillAmount;
     }
-
     public float calculateDiscountAndReturnBillAmount(float totalBillAmount) {
         float discount = (totalBillAmount * 10) / 100;
         float totalDiscountedBill = totalBillAmount - discount;
-        System.out.println("The Discounted Bill Amount is : " + "$" + totalDiscountedBill);
         return totalDiscountedBill;
     }
-
     public void displayOrderDetails(int noOfPizza, int noOfGarlicBread, int noOfBeverage, int size, float totalBillAmount,
-                                    float totalPriceAfterDiscounted) {
+                                    float totalDiscountedBillAmount) {
+        System.out.println("The number of pizzas ordered : " + noOfPizza);
+        System.out.println("The number of garlic bread ordered : " + noOfGarlicBread);
+        System.out.println("The number of beverages ordered : " + noOfBeverage);
+        System.out.println("The Total Bill Amount is : " + "$" +totalBillAmount);
+        System.out.println("The Discounted Bill Amount is : " + "$" + totalDiscountedBillAmount);
     }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        PizzaOrder pizzaOrder = new PizzaOrder();
         System.out.println("Customer Details      ");
         System.out.println("----------------------");
-        String name = input.next();
-        System.out.println("Name of the Customer is : " + name);
-        String email = input.next();
-        System.out.println("Email of the Customer is : " + email);
-		input.nextLine();
-		String address = input.nextLine();
-        System.out.println("Address of the Customer is : " + address);
+        String customerName = input.next();
+        String customerEmail = input.next();
+        String customerAddress = input.next();
         long phoneNumber = input.nextLong();
-        System.out.println("Contact No of the Customer is : " + phoneNumber);
-        System.out.println("-----------------------------------------------");
-        System.out.println("Order Details ");
-        System.out.println("----------------");
-        PizzaOrder pizzaOrder = new PizzaOrder();
-        int size= 0;
         int option;
+        int noOfPizza = 0;
+        int size = 0;
+        int noOfGarlicBread =0;
+        int noOfBeverage=0;
         float totalPizzaPrice = 0;
+        float totalBeveragePrice = 0;
         float totalGarlicBreadPrice = 0;
-        float totalBeverageBill = 0;
+        float totalBillAmount = 0;
+        float totalDiscountedBillAmount =0;
 
-        do {
-            //System.out.println("Enter 1 for Pizza, 2 for Garlic Bread and 3 for Beverages");
+        do{
+            //Enter the option to select from the menu;
             option = input.nextInt();
-
-            switch (option) {
-                case 1:
-                    float priceOfPizza = 0;
-                    if (size == 1) {
-                        priceOfPizza = 9.99f;
-                    } else if (size == 2) {
-                        priceOfPizza = 5.99f;
-                    } else if (size == 3) {
-                        priceOfPizza = 1.99f;
-                    } else {
-                        //System.out.println("Invalid size, Enter 1,2 or 3");
-                        priceOfPizza = 0;
-                    }
-                    size = input.nextInt();
-                    int noOfPizza = input.nextInt();
-                    System.out.println("The number of pizzas ordered : " + noOfPizza);
-                    priceOfPizza = pizzaOrder.getPriceOfPizzaBasedOnSize(size);
-                    totalPizzaPrice = totalPizzaPrice + (pizzaOrder.calculatePriceOfPizza(noOfPizza, priceOfPizza));
+            switch(option) {
+                case 1:{
+                    noOfPizza = input.nextInt();
+                    int size = input.nextInt();
+                    float priceOfPizza = pizzaOrder.getPriceOfPizzaBasedOnSize(size);
+                    pizzaOrder.calculatePriceOfPizza(noOfPizza,priceOfPizza);
                     break;
-                case 2:
-                    int noOfGarlicBread = input.nextInt();
-                    System.out.println("The number of garlic bread ordered : " + noOfGarlicBread);
+                }
+                case 2: {
+                    noOfGarlicBread = input.nextInt();
                     float priceOfGarlicBread = pizzaOrder.getPriceOfGarlicBread();
-                    totalGarlicBreadPrice = totalGarlicBreadPrice + (pizzaOrder.calculatePriceOfGarlicBread(noOfGarlicBread, priceOfGarlicBread));
-                    //System.out.println("Your total Garlic Bread Bill : " + " $ " + totalGarlicBreadBill);
+                    pizzaOrder.calculatePriceOfGarlicBread(noOfGarlicBread,priceOfGarlicBread);
                     break;
-                case 3:
-                    int noOfBeverage = input.nextInt();
-                    System.out.println("The number of beverages ordered : " + noOfBeverage);
+                }
+                case 3: {
+                    noOfBeverage = input.nextInt();
                     float priceOfBeverage = pizzaOrder.getPriceOfBeverage();
-                    totalBeverageBill = totalBeverageBill + (pizzaOrder.calculatePriceOfBeverage(noOfBeverage, priceOfBeverage));
-                    //System.out.println("Your total Beverage Bill : " + " $ " + totalBeverageBill);
+                    pizzaOrder.calculatePriceOfBeverage(noOfBeverage,priceOfBeverage);
                     break;
-            }
-            //System.out.println("Do you want to place another order enter 1 to continue or 0 to exit");
+                }
+            }//press enter 1 to order again or 0 to exit
             option = input.nextInt();
-        }while (option != 0);
-        System.out.println("----------------------------------------------------------------------------------------");
-        float totalBillAmount = pizzaOrder.calculateTotalBill(totalPizzaPrice,totalGarlicBreadPrice,totalBeverageBill);
-        if(totalBillAmount > 50){
-            float discountedBill = pizzaOrder.calculateDiscountAndReturnBillAmount(totalBillAmount);
-        }else {
+        } while(option!=0);
+        pizzaOrder.calculateTotalBill(totalPizzaPrice,totalGarlicBreadPrice,totalBeveragePrice);
+        if (totalBillAmount > 50){
+            pizzaOrder.calculateDiscountAndReturnBillAmount(totalBillAmount);
+        } else {
             System.out.println("No Discount on Bill");
         }
+		System.out.println("Customer Details");
+		System.out.println("----------------");
+        pizzaOrder.displayCustomerDetails(customerName,customerEmail,phoneNumber,customerAddress);
+		System.out.println("----------------------------------------");
+		System.out.println("Order Details  ");
+		System.out.println("---------------");
+        pizzaOrder.displayOrderDetails(noOfPizza,noOfGarlicBread,noOfBeverage,size,totalBillAmount,totalDiscountedBillAmount);
     }
 }
-
