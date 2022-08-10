944fa6128e0cfe3459b24058c3165bbe4f9c947d
seethalakshmi1607 <seethalakshmi1607@gmail.com>
2022-08-10 09:18:30 +0000

    Saving changes from StackRoute workbench on Wed Aug 10 2022 14:48:27 GMT+0530 (India Standard Time)

--- PizzaOrder.java
+++ PizzaOrder.java
@@ -4,9 +4,9 @@ public class PizzaOrder {
 		Scanner input = new Scanner(System.in);
 		String name = input.next();
 		String email = input.next();
-		String address = input.nextLine();
-		input.nextLine();
 		long phoneNo = input.nextLong();
+		input.nextLine();
+		String address = input.nextLine();
 		
 		PizzaBill pizzaBill = new PizzaBill();
 		pizzaBill.displayCustomerDetails(name,email,phoneNo,address);
